1c2e633f41be66c777c45504cd6d41910c31ebd5
young970102 <60802735+young970102@users.noreply.github.com>
2021-12-12 02:25:31 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -1,4 +1,5 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+//let bimages = [""]
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgimage = document.createElement("img");
--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const gretting = document.querySelector("#greeting");
+const logOutForm = document.querySelector("#logOut");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -10,12 +11,21 @@ function paintingGretting(username) {
   gretting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+function onLogOutSubmit(){
+  localStorage.removeItem(USERNAME_KEY);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  logOutForm.classList.add(HIDDEN_CLASSNAME);
+  gretting.classList.add(HIDDEN_CLASSNAME);
+  loginInput.value ="";  
+}
+
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const yourName = loginInput.value;
   localStorage.setItem(USERNAME_KEY, yourName);
   paintingGretting(yourName);
+  logOutForm.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -23,6 +33,9 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
+  logOutForm.classList.remove(HIDDEN_CLASSNAME);
 } else {
   paintingGretting(savedUsername);
 };
+
+logOutForm.addEventListener("click", onLogOutSubmit);

777a86ed069eb85951188c37a40450967b84c673
young970102 <60802735+young970102@users.noreply.github.com>
2021-12-11 14:16:00 +0900

    Delete nomad directory

--- nomad/js/background.js
+++
@@ -1,7 +0,0 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgimage = document.createElement("img");
-bgimage.src = `image/${chosenImage}`;
-
-document.body.appendChild(bgimage);
